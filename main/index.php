cbe71ab5b1c03c2be460b8d6a212e82d65a61556
Naikintojas <rokgytis@gmail.com>
2020-04-16 13:29:16 +0300

    Projektinis darbas

---
+++ main/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="../css/bootstrap.css">
+<link rel="stylesheet" href="../css/bootstrap-grid.css">
+<link rel="stylesheet" href="../css/gridstack.min.css">
+<link rel="stylesheet" href="../style.css">
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/gridstack.js"></script>
+<script type="text/javascript" src="../js/bootstrap.js" ></script>
+<head>
+    <title>Klases vinjiete</title>
+</head>
+<body style="overflow:hidden;">
+<nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Klases vinjiete</a>
+</nav>
+    <div class="bg-secondary w-100">
+    <div class="row">
+        <div class="col-sm-12 "  >
+            <div class="box" style="padding: 20px">
+                <div class="grid-stack" data-gs-column="12" >
+                    <div class="grid-stack-item card"
+                        data-gs-x="0"
+                        data-gs-y="0"
+                        data-gs-width="4"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content  bg-white text-center" style="overflow: hidden">
+                            <img src="../image/nuotrauka1.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="4"
+                        data-gs-y="0"
+                        data-gs-width="4"
+                        data-gs-height="4"
+                    >
+                        <div class="grid-stack-item-content  bg-white text-center" style="overflow: hidden">
+                            <img src="../image/nuotrauka2.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="8"
+                        data-gs-y="0"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                        data-gs-min-width="2"
+                    >
+                        <div class="grid-stack-item-content  bg-white text-center" style="overflow: hidden">
+                            <img src="../image/nuotrauka3.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="10"
+                        data-gs-y="0"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content bg-white text-center" style="overflow: hidden">
+                            <img src="../image/nuotrauka4.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="0"
+                        data-gs-y="2"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content bg-white text-center" style="overflow: hidden">
+                            <img src="../image/nuotrauka5.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="2"
+                        data-gs-y="2"
+                        data-gs-width="2"
+                        data-gs-height="4"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white"style="overflow: hidden">
+                            <img src="../image/nuotrauka6.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="8"
+                        data-gs-y="2"
+                        data-gs-width="4"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white" style="overflow: hidden">
+                            <img src="../image/nuotrauka7.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="0"
+                        data-gs-y="4"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white" style="overflow: hidden">
+                            <img src="../image/nuotrauka1.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="4"
+                        data-gs-y="4"
+                        data-gs-width="4"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white" style="overflow: hidden">
+                            <img src="../image/nuotrauka2.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="8"
+                        data-gs-y="4"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white"style="overflow: hidden">
+                            <img src="../image/nuotrauka3.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div
+                        class="grid-stack-item card"
+                        data-gs-x="10"
+                        data-gs-y="4"
+                        data-gs-width="2"
+                        data-gs-height="2"
+                    >
+                        <div class="grid-stack-item-content text-center bg-white" style="overflow: hidden">
+                            <img src="../image/nuotrauka4.jpg" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+<footer class=" text-center py-3 fixed-bottom bg-dark text-white">
+    © 2020 Gytis Ramanasukas
+</footer>
+</body>
+<script type="text/javascript">
+    $('.grid-stack').gridstack();
+</script>
+</html>
